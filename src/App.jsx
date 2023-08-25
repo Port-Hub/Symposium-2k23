8e0356d44cca49084e2de39f7359b525615e2893
Amala Shwetah <93658002+Amala02@users.noreply.github.com>
2023-08-25 22:30:59 +0530

    cursor-added

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import React from "react";
 import Cards from "./components/Cards";
 import Flakes from "./components/Background";
 
+
 const App = () => {
   return (
     <div>
       <Flakes />
       <Cards />
+      
     </div>
   );
 };
