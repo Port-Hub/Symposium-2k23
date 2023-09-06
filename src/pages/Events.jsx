d2bb47fc7a83575976cf5eaf9593127adc1f6731
Amala Shwetah <93658002+Amala02@users.noreply.github.com>
2023-09-06 21:41:43 +0530

    music changes

--- src/pages/Events.jsx
+++ src/pages/Events.jsx
@@ -3,11 +3,14 @@ import React from "react";
 import Flakes from "../components/Background";
 import Cards from "../components/Cards";
 import eventDetails from "../components/eventDetails";
+import AudioPlayer from "../../public/audio/audio";
 const Events = () => {
   return (
     <div>
+      
       <Flakes />
       <Cards cardData={eventDetails} />
+      <AudioPlayer/>
     </div>
   );
 };
